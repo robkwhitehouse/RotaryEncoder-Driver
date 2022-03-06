4b26d21d0155e6153692728d7291baa8821ea8a4
Robert Whitehouse <62645132+robkwhitehouse@users.noreply.github.com>
2022-03-06 20:42:58 +0000

    Add files via upload

--- RotaryEncoder.hpp
+++ RotaryEncoder.hpp
@@ -22,13 +22,22 @@
   This driver assumes that anything with duration greater than 5ms is a valid pulse and
   ignores anything with shorter duration. This can be tweaked if necessary (see below). 
   
-  The driver is not interrupt driven and the "scan() method should be called every  millisecond 
-  (you might get awy with doing this every 2ms)
+  The driver uses two interrupts, one for the rotary pulses  and one
+  for the push button. Either interrupt will put the encoder into the "active"
+  state. While active the "scan() method should be called at (max) 3ms intervals
+  
+  The number of rotary pulses counted is artifically incremented if the 
+  knob is rotated quickly.  
   
 */
  
-#define MIN_PERIOD 5  // milliseconds
+#define DEBOUNCE_INTERVAL 5000  // 5 milliseconds
+#define LONG_PRESS_INTERVAL 3000000 //3 seconds
+#define ACTIVITY_TIMEOUT 10000000 //10 seconds
+#define BUTTON_UP false
+
 #include "TaskScheduler.h"
+#include "StateMachine.hpp"
 
 //Forward declarations
 class RotaryEncoder;
@@ -43,19 +52,22 @@ class RotaryEncoder {
    public:
      //  -- constructor
      RotaryEncoder(uint8_t _pinA, uint8_t _pinB, uint8_t _pinC) {
-       pinA = _pinA;
-       pinB = _pinB;
-       pinC = _pinC;
-       pinMode(pinA,INPUT_PULLUP);
-       pinMode(pinB,INPUT_PULLUP);
-       pinMode(pinC,INPUT_PULLUP);
-       instance = this;         
+       pinA = _pinA; //Rotary "data" 
+       pinB = _pinB; //Rotary "clock"
+       pinC = _pinC; //Pushbutton
+
+       instance = this;  //Needed by intrrupt handlers        
      }
 
 //Must call this during setup()     
-     void begin() { 
-       attachInterrupt(digitalPinToInterrupt(pinA), encoderIntHandler, FALLING); 
-       attachInterrupt(digitalPinToInterrupt(pinC), buttonIntHandler, LOW);
+     void begin(bool _accel=true) { 
+       pinMode(pinA,INPUT_PULLUP);
+       pinMode(pinB,INPUT_PULLUP);
+       pinMode(pinC,INPUT_PULLUP);
+       accel = _accel;
+       attachInterrupt(digitalPinToInterrupt(pinA), encoderIntHandler, RISING); //rotary motion are we only inetrested in one edge
+       attachInterrupt(digitalPinToInterrupt(pinC), buttonIntHandler, CHANGE); //push button - we want to time down and up
+       
      }        
 
 //Returns number of clicks since previous call     
@@ -64,39 +76,132 @@ class RotaryEncoder {
        pulseCount = 0;
        return(retVal);
      }
+     
+//Returns true if there has been recent activity (in last 60 secs)
+    bool isActive() {
+      return(active);
+    }
+    
+//Called every time through loop() if encoder is active - must be non-blocking and quick
+    void scan() {
+      long now = 0;
+      
+      //What time is it now?
+      now = micros();
+      
+      //Check for recent activity
+      if ( now > lastActivity + ACTIVITY_TIMEOUT || now < lastActivity ) {
+        active = false;
+        lastActivity = 0;
+      }
+    
+      //Check for end of de-bounce interval
+      if (inDebounceDelay) {
+        if ( now > deBounceEnd ) {
+          inDebounceDelay = false; 
+        } else return; //In debounce - ignore all events
+      }
 
-//Allow ISR access to members
+      //Has the button state changed? (recorded by int handler);
+      if (buttonDown != buttonState ) {
+      Serial.println("Button change");
+        if (buttonDown) 
+          pressStart = now; //New button press started
+        else 
+          pressEnd = now; 
+        if (!buttonDown) { //Button released
+          if ( now - pressStart > LONG_PRESS_INTERVAL )
+            encoderEvent = LONGPRESS;
+          else
+            //Short press event
+            encoderEvent = SHORTPRESS;
+          eventQueue.push(&encoderEvent);
+        } 
+        buttonState = buttonDown; //Save current state         
+      }
+    } // End of scan() method
+    
+    void dumpState() { //output state variables (for debug)
+      char buff[128];
+      sprintf(buff, "active: %d, lastActivity %d, inDebounceDelay: %d, buttonDown: %d, buttonState: %d\n",
+          active,lastActivity,inDebounceDelay,buttonDown,buttonState);
+      Serial.print(buff);
+    }
+
+     //Allow ISR access to members
      friend void encoderIntHandler();
+     friend void buttonIntHandler();
 
+     //Properties
      volatile int pulseCount;
      uint8_t pinA, pinB, pinC; 
-     volatile long deBounceStart, deBounceEnd;
+     volatile long deBounceEnd, lastActivity, rotaryPulseStart;
      volatile bool inDebounceDelay = false;
-};
+     volatile bool active = false;
+     volatile bool buttonDown = BUTTON_UP;
+     volatile bool buttonState = BUTTON_UP;
+     long pressStart, pressEnd;  //Measures button press
+     bool accel = true;
+     bool pulseStarted = false;
+}; //end of RotaryEncoder class definition
 
+//Interrupt Handlers
 
+//Called on edge (on pinA) - rotary motion
 static void encoderIntHandler() {
-       int pinBval;  
-       
-   if ( instance->inDebounceDelay == false ) { 
-       // initiate de-bounce delay (ignore further interrupts for 2ms)
-       instance->inDebounceDelay = true;
-       instance->deBounceStart = micros();
-       instance->deBounceEnd = instance->deBounceStart + 1000;   
+   int pinBval, increment;
+   long now, pulseDuration; 
+   bool pulseReceived;
+    
+   now = micros();
+   instance->active = true;
+   instance->lastActivity = now;    //Start activity timer
+   
+   //Main body only executed if not in de-bounce period
+   if ( instance->inDebounceDelay == false ) {
+   
+       // initiate de-bounce delay and set end time
+       instance->inDebounceDelay = true;  //DebounceDelay is terminated in scan()
+       instance->deBounceEnd = now + DEBOUNCE_INTERVAL; 
+      
+       // Work out if we have received a complete pulse
+       pulseReceived = false;
+       if (!instance->pulseStarted) { //start of pulse
+         instance->pulseStarted = true;
+         instance->rotaryPulseStart = now;
+       } else { //end of pulse
+         instance->pulseStarted = false;
+         pulseDuration = now - instance->rotaryPulseStart;
+         pulseReceived = true;
+       }
+  
        // --- Get the current pin states and increment pulseCount accordingly
+       increment = 1;
+       if ( pulseReceived ) {
+         if(instance->accel)  //calculate increment        
+           increment = 1 + (1000000 / (3*pulseDuration)); //If using encoder speed add a factor 
+       }
+           
+       //Need to look at the CLK pin to work out diretion of rotation  
        pinBval = digitalRead(instance->pinB);
-       if ( pinBval == 0 ) instance->pulseCount++; //clockwise rotation
-       else instance->pulseCount--;
+       if ( pinBval == 0 ) instance->pulseCount += increment; //clockwise rotation
+       else instance->pulseCount -= increment;
+       if (instance->pulseCount < 0) instance->pulseCount = 0;
    }
 }
 
+//Called on falling and rising edges of the button pin
 static void buttonIntHandler() {
-
+  long now;
+  
+  instance->active = true;
   if ( instance->inDebounceDelay == false ) { 
-       // initiate de-bounce delay (ignore further interrupts for 2ms)
+       // initiate de-bounce delay (ignore further interrupts for a while)
        instance->inDebounceDelay = true;
-       instance->deBounceStart = micros();
-       instance->deBounceEnd = instance->deBounceStart + 1000;
+       now = micros();
+       instance->lastActivity = now;
+       instance->deBounceEnd = now + DEBOUNCE_INTERVAL;
+       instance->buttonDown = !digitalRead(instance->pinC); //record current button position, up or down
    }
 }       
 
